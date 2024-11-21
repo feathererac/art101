5c8895826d9633f17768f10caee567e9a028ff10
ZStevLin <lzfj77889@gmail.com>
2024-11-20 16:40:48 -0800

    lab 14 Debug

---
+++ lab14/js/lab.js
@@ -0,0 +1,14 @@
+/*
+Author: Zhongfengjing Lin
+Date: November, 2024
+Requirements: jQuery must be loaded for this script to work.
+License: Public Domin
+*/
+
+//Function
+$("#home-button").click(function() {
+  window.location.href = "../index.html";
+});
+
+
+
